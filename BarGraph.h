50f9461f1a90bde4fe178ddab4414799bc2ddc28
tablatronix <spam@rumental.com>
2019-09-24 22:20:43 -0500

    convert to adafruit lcd , using pointers
    I could not use references for some reason using a pointer for now..

--- BarGraph.h
+++ BarGraph.h
@@ -2,16 +2,19 @@
 #ifndef _BarGraphH_
 #define _BarGraphH_
 
-#include "TFT_eSPI.h"
+// #include "TFT_eSPI.h"
 #include <Arduino.h>
+#include "Adafruit_ILI9341.h" // Library Manager
 
 #define SCALECOLOR 0xFFFF
 
 class BarGraph
 {
+	using adafruit_tft = Adafruit_ILI9341;
   public:
+	adafruit_tft *_tft;
     BarGraph();
-	void begin(int l, int u, int x, int y, TFT_eSPI& tft);
+	void begin(int l, int u, int x, int y, adafruit_tft* tft);
 	void setColor(int bar, int back);
     void drawBar(int nPer);
 	void drawScale();
@@ -27,7 +30,6 @@ class BarGraph
 	 int _backColor;
 	 int _x;
 	 int _y;
-	 TFT_eSPI _tft;
 };
 
 #endif
